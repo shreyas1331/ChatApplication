a127868f8248313eedaa989856579d17076d734f
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2024-04-12 21:35:07 +0530

    new update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-feuu.onrender.com", {
+			const socket = io("https://chatapplication-lckq.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
